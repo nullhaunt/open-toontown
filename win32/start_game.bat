e7d4d1dba9987fcb23cff3dd4d40ece91cfcaa06
Nullhaunt <292233993+nullhaunt@users.noreply.github.com>
2026-06-11 11:55:05 -0700

    win32: start_game.bat allows entering specific token

--- win32/start_game.bat
+++ win32/start_game.bat
@@ -5,7 +5,8 @@ cd..
 rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
-set LOGIN_TOKEN=dev
+set /P LOGIN_TOKEN=Login token [dev]: 
+if "%LOGIN_TOKEN%"=="" set LOGIN_TOKEN=dev
 
 %PPYTHON_PATH% -m toontown.launcher.QuickStartLauncher
 pause
